fd6df00eb9c7bb5b5a7f11e313ccc0fa602cf260
Shivam Goyal <shivam1111@gmail.com>
2017-04-25 21:05:46 +0530

    Removed bug from agecheker

--- static/js/utils.js
+++ static/js/utils.js
@@ -1,4 +1,18 @@
 // using jQuery
+
+function setCookie(name, value, days)
+{
+  if (days)
+  {
+    var date = new Date();
+    date.setTime(date.getTime()+days*24*60*60*1000); // ) removed
+    var expires = "; expires=" + date.toGMTString(); // + added
+  }
+  else
+    var expires = "";
+  document.cookie = name+"=" + value+expires + ";path=/"; // + and " added
+}
+
 function getCookie(name) {
     var cookieValue = null;
     if (document.cookie && document.cookie !== '') {
