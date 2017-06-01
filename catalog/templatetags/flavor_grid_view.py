e2eb51b11facba9f3f509c1ba1acd96907457647
Shivam Goyal <shivam1111@gmail.com>
2017-06-01 08:17:30 +0530

    added request param to flavor_grid_view templatetag as it was not being passed on to template

--- catalog/templatetags/flavor_grid_view.py
+++ catalog/templatetags/flavor_grid_view.py
@@ -4,10 +4,10 @@ from django.shortcuts import get_object_or_404
 register = template.Library()
 
 @register.inclusion_tag("flavor_grid_view.html")
-def flavor_grid_view(flavor,volume_id,back_url):
+def flavor_grid_view(request,flavor,volume_id,back_url):
 	# name is volume name
 	if isinstance(volume_id,ProductAttributeValue):
 		current_volume = volume_id
 	else:	
 		current_volume = get_object_or_404(ProductAttributeValue, pk=volume_id)
-	return {'flavor_id': flavor,'name':current_volume.name,'current_volume':current_volume,'back_url':back_url }
+	return {'request':request,'flavor_id': flavor,'name':current_volume.name,'current_volume':current_volume,'back_url':back_url }
